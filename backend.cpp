3613d0981f750b547eaf0086d9204121a50151dd
Darren K.J. Chen <43134270+darren-k-chen@users.noreply.github.com>
2022-10-23 15:10:39 +0800

    Update backend.cpp

--- backend.cpp
+++ backend.cpp
@@ -4,6 +4,7 @@
 #include <csignal>
 #include <cstdlib>
 #include <ctime>
+#include <bits/stdc++.h>
 
 // [Sys Env.] Ubuntu 20.04 LTS
 // [Required] https://github.com/wfrest/wfrest
@@ -13,16 +14,45 @@
 
 #define SWAP(x,y,t) (t=x, x=y, y=t);
 
+using namespace std;
 using namespace wfrest;
 using Json = nlohmann::json;
 
 clock_t Begin, End;
+
+string perResult = "";
+string hanoiResult = "";
+string magicResult = "";
 
 static WFFacilities::WaitGroup wait_group(1);
 
 void sig_handler(int signo)
 {
     wait_group.done();
+}
+
+void permute(string a, int l, int r) 
+{
+    int tmp;
+    if (l == r) perResult += a + "\n";
+    else for (int i = l; i <= r; i++) 
+    {
+        SWAP(a[l], a[i], tmp);
+        permute(a, l+1, r);
+        SWAP(a[l], a[i], tmp);
+    }
+}
+
+void hanoi(int n, string from, string to, string aux)
+{
+    if (n == 1)
+    {
+        hanoiResult += ( "Move disk 1 from rod " + from + " to rod " + to + "\n" );
+        return;
+    }
+    hanoi(n - 1, from, aux, to);
+    hanoiResult += ( "Move disk " + to_string(n) + " from rod " + from + " to rod " + to + "\n" );
+    hanoi(n - 1, aux, to, from);
 }
 
 Json selectionSort(Json data, int n)
@@ -57,7 +87,7 @@ int main()
 
     HttpServer svr;
 
-    svr.Static("/static", "./forntend");
+    svr.Static("/static", "./frontend");
 
     // CMD: curl -v http://<ip>:<port>/test
     svr.GET("/test", [](const HttpReq *req, HttpResp *resp)
@@ -70,7 +100,7 @@ int main()
     // MEMO: NEED RETURN [randaom number * n] & [Sort the random number via Selection Sort] & [Sort the random number via Bubble Sort]
     // CMD: curl -X POST http://<ip>:<port>/hw1 -H "body-type:application/x-www-form-urlencoded" \ -d 'n=<Int: n>&pswd=<Int: range>'
     svr.POST("/hw1", [](const HttpReq *req, HttpResp *resp)
-    {
+    { // try {
         Json data; std::map<std::string, std::string> &form_kv = req->form_kv();
         
         for (auto &kv: form_kv) data[kv.first] = std::stoi(kv.second);
@@ -83,7 +113,64 @@ int main()
         Begin = clock(); data["bubbleSort"]["data"]    = bubbleSort(data["randomNumber"], n);
         End   = clock(); data["bubbleSort"]["time"]    = (double)(End - Begin) / CLOCKS_PER_SEC;
 
-        resp->Json(data);
+        resp->Json(data); // throw data; } catch (Json data) { data["error"] = "ThrowErr: Exception Caught"; resp->Json(data); }
+    });
+
+    svr.POST("/hw2-hanoi", [](const HttpReq *req, HttpResp *resp)
+    { // try {
+        Json data; std::map<std::string, std::string> &form_kv = req->form_kv();
+        
+        for (auto &kv: form_kv) data[kv.first] = std::stoi(kv.second);
+        int n = data["n"]; hanoiResult = "";
+        hanoi(n, "A", "C", "B"); data["hanoiResult"] = hanoiResult;
+
+        resp->Json(data); // throw data; } catch (Json data) { data["error"] = "ThrowErr: Exception Caught"; resp->Json(data); }
+    });
+
+    svr.POST("/hw3", [](const HttpReq *req, HttpResp *resp)
+    { // try {
+        Json data; std::map<std::string, std::string> &form_kv = req->form_kv();
+        
+        for (auto &kv: form_kv) data[kv.first] = std::stoi(kv.second);
+        int n = data["n"]; magicResult = "";
+        if (n % 2) {
+            int magicSquare[n][n];
+            memset(magicSquare, 0, sizeof(magicSquare));
+            int i = n / 2, j = n - 1;
+            for (int num = 1; num <= n * n;)
+            {
+                if (i == -1 && j == n) {
+                    j = n - 2; i = 0;
+                } else {
+                    if (j == n) j = 0;
+                    if (i < 0) i = n - 1;
+                } if (magicSquare[i][j]) {
+                    j -= 2; i++;
+                    continue;
+                } else magicSquare[i][j] = num++; j++; i--;
+            }
+            
+            magicResult += "Magic Square of size " + to_string(n) + ":\n\n";
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++) magicResult += to_string(magicSquare[i][j]) + " ";
+                magicResult += "\n";
+            }
+        } else magicResult = "n must be odd number!";
+        data["magicResult"] = magicResult;
+        
+        resp->Json(data); // throw data; } catch (Json data) { data["error"] = "ThrowErr: Exception Caught"; resp->Json(data); }
+    });
+
+    svr.POST("/hw2-permutation", [](const HttpReq *req, HttpResp *resp)
+    { // try {
+        Json data; std::map<std::string, std::string> &form_kv = req->form_kv();
+        
+        for (auto &kv: form_kv) data[kv.first] = kv.second;
+        string str = data["str"]; int n = str.length(); perResult = "";
+        permute(str, 0, n-1); data["permutation"] = perResult;
+
+        resp->Json(data); // throw data; } catch (Json data) { data["error"] = "ThrowErr: Exception Caught"; resp->Json(data); }
     });
 
     if (svr.track().start(8888) == 0)
